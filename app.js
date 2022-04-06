0d6d29d31e1addff5cfecb8e7b576ce5531dc313
anpavlov11 <apavlo11@gmail.com>
2022-04-06 14:11:29 -0500

    added Click event faded out of game directions

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-Create object literals for the questions and answers; use an array to make questions easier to iterate over;
-Create an output variable for HTML output;
-Create a forEach loop;
-    Name curentQuestion and questionNumber;
-        Create the array for possible correct answers;
-            Use a loop to fill in answer possibilities)))
-        Create a radio(?) button for each choice in a label element;
-            Create submit button;
-                Use conditional if statement for submit click event---
-                        if timer runs out before submit is clicked;
-                            else submit is clicked;
-                            revealAnswer();
-                        revealAnswer function to determine a correct answer:
-                            if answer is correct
-                                add 5 points to score;
-                            else if answer is incorrect
-                                add 0 points;
-                            else award 0 points;
+// Create object literals for the questions and answers; use an array to make questions easier to iterate over;
+// Create an output variable for HTML output;
+// Create a forEach loop;
+//     Name curentQuestion and questionNumber;
+//         Create the array for possible correct answers;
+//             Use a loop to fill in answer possibilities)))
+//         Create a radio(?) button for each choice in a label element;
+//             Create submit button;
+//                 Use conditional if statement for submit click event---
+//                         if timer runs out before submit is clicked;
+//                             else submit is clicked;
+//                             revealAnswer();
+//                         revealAnswer function to determine a correct answer:
+//                             if answer is correct
+//                                 add 5 points to score;
+//                             else if answer is incorrect
+//                                 add 0 points;
+//                             else award 0 points;
 
-Function for end of game;
-Trigger results
-    Open div for results
-        Conditional
-            if player scores 50 points,
-                console.log genius of useless knowledge;
-            else if player scores 40-49 points,
-                console.log you know a lot of nothing;
-            else if player scores 30-39 points,
-                console.log you know a little of nothing;
-            else if player scores 20-29,
-                console.log keep living, useless knowledge is all around you...remember it
-            else
-                console.log you know nothing of nothing...you must be great fun
+// Function for end of game;
+// Trigger results
+//     Open div for results
+//         Conditional
+//             if player scores 50 points,
+//                 console.log genius of useless knowledge;
+//             else if player scores 40-49 points,
+//                 console.log you know a lot of nothing;
+//             else if player scores 30-39 points,
+//                 console.log you know a little of nothing;
+//             else if player scores 20-29,
+//                 console.log keep living, useless knowledge is all around you...remember it
+//             else
+//                 console.log you know nothing of nothing...you must be great fun
 
-click event reset/new game;
+// click event reset/new game;
+
+const target = document.getElementById("target");
+
+target.addEventListener('click', () => target.style.opacity = '0');
+target.addEventListener('transitionend', () => target.remove());
