4a88e2d7927f9466656446d6a480d86fe716b67b
anpavlov11 <apavlo11@gmail.com>
2022-04-08 13:46:57 -0500

    Early afternoon commit-using a node modul for transition from directions page to game page..updated all pages to function properly

--- modules/canvas.js
+++ modules/canvas.js
@@ -1,6 +1,6 @@
-const target = document.querySelector(".directions");
+// const target = document.querySelector(".directions");
 
-target.addEventListener('click', () => target.style.opacity = '0');
-target.addEventListener('transitionend', () => target.remove());
+// target.addEventListener('click', () => target.style.opacity = '0');
+// target.addEventListener('transitionend', () => target.remove());
 
-export {target};
+// export {target};
