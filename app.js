89910e165cef43c310971563b0f8935a12234261
anpavlov11 <apavlo11@gmail.com>
2022-04-11 10:35:44 -0500

    Break1 commit - completed all trivia questions, answers and answer options

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// let myCanvas = target('myCanvas', document.body, 300, 700);
-
 const useKnow = document.querySelector('.asksandanswers');
 const questions = [{
     ask: 'Who was the first US Secretary of the Treasury?', answer: 'Alexander Hamilton',
     options: [{title: 'Aaron Burr'}, {title: 'John Adams'}, {title: 'Alexander Hamilton'}], 
     ask: 'What is the only horror movie to win the Academy Award for Best Picture?', answer: 'Silence of the Lambs',
     options: [{title: 'Silence of the Lambs'}, {title: 'Halloween'}, {title: 'The Shining'}], 
-    ask: ''
+    ask: 'What is the name of the treaty that ended World War I?', answer: 'The Treaty of Versailles',
+    options: [{title: 'The Vienna Convention'}, {title: 'The Paris Peace Treaties'}, {title: 'The Treaty of Versailles'}],
+    ask: 'What was the top-grossing movie of the 1990s?', answer: 'Titanic',
+    options: [{title: 'Star Wars: Episode I - The Phantom Menace'}, {title: 'Titanic'}, {title: 'Jurassic Park'}],
+    ask: 'What is the order of the first 5 books of the Bible?', answer: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy',
+    options: [{title: 'Genesis, Numbers, Deuteronomy, Leviticus, Exodus'}, {title: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy'}, {title: 'Genesis, Numbers, Exodus, Leviticus, Deuteronomy'}],
+    ask: 'Elvis Presley had a twin brother. What was his name?', answer: 'Jessie',
+    options: [{title: 'Jessie'}, {title: 'Aron'}, {title: 'Vernon'}],
+    ask: 'How long is four score and seven years?', answer: '87',
+    options: [{title: '27'}, {title: '69'}, {title: '87'}],
+    ask: 'How many legs do dragonflies have?', answer: '6',
+    options: [{title: '6'}, {title: '8'}, {title: '2'}],
+    ask: 'How many hearts do octopuses and squid have?', answer: '3',
+    options: [{title: '1'}, {title: '3'}, {title: '0'}],
+    ask: 'What is the capital of Canada?', answer: 'Ottawa',
+    options: [{title: 'Ottawa'}, {title: 'Winnipeg'}, {title: 'Toronto'}],
     },]
 // Create object literals for the questions and answers; use an array to make questions easier to iterate over;
 // Create an output variable for HTML output;
