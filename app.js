230e2e227b1a4b7bb05bd2a0b6a094074a3de08d
anpavlov11 <apavlo11@gmail.com>
2022-04-12 23:04:17 -0500

    Last commit

--- app.js
+++ app.js
@@ -1,121 +1,134 @@
-const useKnow = document.querySelector('.asksAndOptions');
+const uselessKnowledge = document.querySelector('.trivia');
 const questions = [{
-    ask: 'Who was the first US Secretary of the Treasury?', answer: 'Alexander Hamilton',
-    options: [{title: 'Aaron Burr'}, {title: 'John Adams'}, {title: 'Alexander Hamilton'}], 
-    ask: 'What is the only horror movie to win the Academy Award for Best Picture?', answer: 'Silence of the Lambs',
-    options: [{title: 'Silence of the Lambs'}, {title: 'Halloween'}, {title: 'The Shining'}], 
-    ask: 'What is the name of the treaty that ended World War I?', answer: 'The Treaty of Versailles',
-    options: [{title: 'The Vienna Convention'}, {title: 'The Paris Peace Treaties'}, {title: 'The Treaty of Versailles'}],
-    ask: 'What was the top-grossing movie of the 1990s?', answer: 'Titanic',
-    options: [{title: 'Star Wars: Episode I - The Phantom Menace'}, {title: 'Titanic'}, {title: 'Jurassic Park'}],
-    ask: 'What is the order of the first 5 books of the Bible?', answer: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy',
-    options: [{title: 'Genesis, Numbers, Deuteronomy, Leviticus, Exodus'}, {title: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy'}, {title: 'Genesis, Numbers, Exodus, Leviticus, Deuteronomy'}],
-    ask: 'Elvis Presley had a twin brother. What was his name?', answer: 'Jessie',
-    options: [{title: 'Jessie'}, {title: 'Aron'}, {title: 'Vernon'}],
-    ask: 'How long is four score and seven years?', answer: '87',
-    options: [{title: '27'}, {title: '69'}, {title: '87'}],
-    ask: 'How many legs do dragonflies have?', answer: '6',
-    options: [{title: '6'}, {title: '8'}, {title: '2'}],
-    ask: 'How many hearts do octopuses and squid have?', answer: '3',
-    options: [{title: '1'}, {title: '3'}, {title: '0'}],
-    ask: 'What is the capital of Canada?', answer: 'Ottawa',
-    options: [{title: 'Ottawa'}, {title: 'Winnipeg'}, {title: 'Toronto'}],
-    },];
+  name: 'Who was the first US Secretary of the Treasury?', answer: 'Alexander Hamilton',
+  options: [{ title: 'Aaron Burr' }, { title: 'John Adams' }, { title: 'Alexander Hamilton' }, { title: 'Oliver Wolcott, Jr.' } ]
+}, {
+   name: 'What is the name of the treaty that ended World War I', answer: 'The Treaty of Versailles',
+  options: [{ title: 'The Vienna Convention' }, { title: 'The Paris Peace Treaties' }, { title: 'The Treaty of Versailles' }, { title: 'The German Treaty' } ]
+}, {
+   name: 'What was the top-grossing movie of the 1990s?', answer: 'Titanic',
+  options: [{ title: 'Toy Story' }, { title: 'Star Wars: Episode I - The Phantom Menace' }, { title: 'Titanic' }, { title: 'Jurassic Park' } ]
+}, {
+   name: 'What is the order of the first 5 books of the Bible?', answer: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy',
+  options: [{ title: 'Genesis, Numbers, Deuteronomy, Leviticus, Exodus' }, { title: 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy' }, { title: 'Genesis, Numbers, Exodus, Leviticus, Deuteronomy' }, { title: 'Exodus, Genesis, Leviticus, Numbers, Deuteronomy' } ]
+}, {
+   name: 'Elvis Presley had a twin brother. What was his name?', answer: 'Jessie',
+  options: [{ title: 'Jessie' }, { title: 'Aron' }, { title: 'Vernon' }, { title: 'Vince' } ]
+}, {
+   name: 'How long is four score and seven years?', answer: '87 years',
+  options: [{ title: '27 years' }, { title: '69 years' }, { title: '87 years' }, { title: '107 years' } ]
+}, {
+   name: 'How many legs do dragonflies have?', answer: '6',
+  options: [{ title: '6' }, { title: '8' }, { title: '2' }, { title: '4' } ]
+}, {
+   name: 'How many hearts do octopuses and squid have?', answer: '3',
+  options: [{ title: '1' }, { title: '3' }, { title: '0' }, { title: '8' } ]
+}, {
+   name: 'What is the capital of Canada?', answer: 'Ottawa',
+  options: [{ title: 'Ottawa' }, { title: 'Winnipeg' }, { title: 'Toronto' }, { title: 'Montreal' } ]
+}, {
+  name: 'What is the only horror movie to win the Academy Award for Best Picture?', answer: 'The Silence of the Lambs',
+  options: [{ title: 'The Silence of the Lambs' }, { title: 'Halloween' }, { title: 'The Shining' }, { title: 'The Amityville Horror' } ]
+}];
 
-    document.querySelector('.asksAndOptions').style.display = "table";
+document.querySelector('.trivia').style.display = "table";
 
-    let thisQuestion = 0;
-    let answered = false;
+let thisQuestion = 0;
+let submitted = false;
 
-    const giveQuestion = () => {
-        answered = false;
+const displayQuestion = () => {
+  submitted = false;
 
-        removeEventListener(useKnow.querySelector('.nextQuestion'), handleNext);
-        removeEventListener(useKnow.querySelector('.answer'), handleAnswer);
+  removeEventListener(uselessKnowledge.querySelector('#nextQuestion'), handleNext);
+  removeEventListener(uselessKnowledge.querySelector('#submit'), handleSubmit);
 
-        const question =  questions[thisQuestion];
-        const lastOne = thisQuestion === questions.length - 1;
+  const question = questions[thisQuestion];
+  const lastOne = thisQuestion === questions.length - 1;
 
-        useKnow.innerHTML = `
-            <h3>${question.ask}</h3>
-            <form id="myForm">
-                <input type="radio" id="0" ask="option" value="0">
-                <label for="0">${question.options[0].title}</label><br>
+  uselessKnowledge.innerHTML = `
+    <h3>${question.name}</h3>
+    <form id="myForm">
+        <input type="radio" id="0" name="option" value="0">
+        <label for="0">${question.options[0].title}</label><br>
 
-                <input type="radio" id="1" ask="option" value="1">
-                <label for="1">${question.options[1].title}</label><br>
+        <input type="radio" id="1" name="option" value="1">
+        <label for="1">${question.options[1].title}</label><br>
 
-                <input type="radio" id="2" ask="option" value="2"></input>
-                <label for="2">${question.options[2].title}</label><br>
+        <input type="radio" id="2" name="option" value="2">
+        <label for="2">${question.options[2].title}</label><br>
 
-                <p class="text"></p>
+        <input type="radio" id="3" name="option" value="3">
+        <label for="3">${question.options[3].title}</label><br>
 
-                <button class="answer">Answer</button>
-                <button class="nextQuestion">${!lastOne ? 'Answer' : 'Next'}</button>
-            </form>
-            `;
+        <p id="message"></p>
 
-        addEventListener(useKnow.querySelector('.nextQuestion'), handleNext);
-        addEventListener(useKnow.querySelector('.answer'), handleAnswer);
-        
-    }
+        <button id="submit">Submit</button>
+        <button id="nextQuestion">${!lastOne ? 'Next' : 'Finish'}</button>
+    </form> 
+  `;
 
-    const addEventListener = (btn, event) => {
-        if (btn) btn.addEventListener('click', event);
-    };
+  addEventListener(uselessKnowledge.querySelector('#nextQuestion'), handleNext);
+  addEventListener(uselessKnowledge.querySelector('#submit'), handleSubmit);
+}
 
-    const removeEventListener = (btn, event) => {
-        if (btn) btn.removeEventListener('click', event)
-    };
+const addEventListener = (btn, listener) => {
+  if (btn) btn.addEventListener('click', listener);
+};
 
-    const handleNext = event => {
-        event.preventDefault();
+const removeEventListener = (btn, listener) => {
+  if (btn) btn.removeEventListener('click', listener);
+};
 
-        const msg = document.querySelector('.message');
+const handleNext = e => {
+  e.preventDefault();
+  
+  const msg = document.getElementById('message');
+  
+  if (document.querySelector('input[name="option"]:checked') === null) {
+    msg.innerHTML = "Select an answer to proceed."
+    return; 
+  }
+  
+  if (!submitted) {
+    msg.innerHTML = "Please submit your answer before proceeding."
+    return; 
+  }
+  
+  if (thisQuestion < questions.length - 1) {
+    thisQuestion++;
+    displayQuestion();
+  } else {
+    msg.innerHtml = "That was the last question.";
+  }
+};
 
-        if (document.querySelector('input [ask = "option"] : checked') === null) {
-            msg.innerHTML = "You have to choose an answer to continue."
-            return;
-        }
+const handleSubmit = e => {
+  e.preventDefault()
 
-        if (!answered) {
-            msg.innerHTML = "Press 'Next' to continue."
-            return;
-        }
+  const msg = document.getElementById('message');
+  const ans = document.querySelector('input[name="option"]:checked');
 
-        if (thisQuestion < questions.length - 1) {
-            thisQuestion++;
-            giveQuestion();
-        } else {
-            alert("Last one!");
-        }
-    };
+  if (!ans) {
+    msg.innerHTML = "Please select an answer."
+    return; 
+  }
 
-    const handleAnswer = event => {
-        const msg = document.getElementById('message');
-        const ans = document.getElementById('input [ask = "option"] : checked');
+  const question = questions[thisQuestion];
+  const correctAns = question.answer;
+  const userAns = ans.value;
 
-        if (!ans) {
-            msg.innerHTML = "Pick an answer."
-            return;
-        }
+  if (correctAns === userAns) {
+    msg.innerHTML = "Correct"
+  } else {
+    msg.innerHTML = "Incorrect"
+  }
 
-        const question = questions[thisQuestion];
-        const correctAns = question.answer;
-        const userAns = ans.value;
+  document.getElementById("0").disabled = true;
+  document.getElementById("1").disabled = true;
+  document.getElementById("2").disabled = true;
+  document.getElementById("3").disabled = true;
+  
+  submitted = true;
+};
 
-        if (correctAns === userAns) {
-            msg.innerHTML = "Yes!!"
-        } else {
-            msg.innerHTML = "Nope."
-        }
-
-        document.getElementById("0").disabled = true;
-        document.getElementById("1").disabled = true;
-        document.getElementById("2").disabled = true;
-
-        answered = true;
-    };
-
-    giveQuestion();
-    Event.preventDefault();
+displayQuestion();
